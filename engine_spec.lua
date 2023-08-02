f16cf7691993f60f5e677980df7c79efb1a9a474
Jim Barnett <james.barnett.jr@gmail.com>
2023-08-02 01:05:41 -0500

    More arithmetic tests

--- engine_spec.lua
+++ engine_spec.lua
@@ -1,10 +1,37 @@
 require "engine"
 
 describe('Value', function()
-	it('adds two Values', function()
-		local a = Value:new(2.0)
-		local b = Value:new(-4.0)
-		local c = a + b
-		assert.equal(c.data, -2.0)
+	describe('Addition', function()
+		it('works with two Values', function()
+			local a = Value:new(2.0)
+			local b = Value:new(-4.0)
+			local c = a + b
+			assert.equal(c.data, -2.0)
+		end)
+
+		it('works with simple numbers', function()
+			local a = Value:new(2.0)
+			local b = a + 5.0
+			assert.equal(b.data, 7.0)
+			local c = 3.0 + a
+			assert.equal(c.data, 5.0)
+		end)
+	end)
+
+	describe('Multlplication', function()
+		it('works with two Values', function()
+			local a = Value:new(4.0)
+			local b = Value:new(7.0)
+			local c = a * b
+			assert.equal(c.data, 28.0)
+		end)
+
+		it('works with simple numbers', function()
+			local a = Value:new(2.5)
+			local b = a * 4
+			assert.equal(b.data, 10.0)
+			local c = 2 * a
+			assert.equal(c.data, 5.0)
+		end)
 	end)
 end)
